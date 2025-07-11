b8fd356fade215b27c6c51e194928f1b9b14f338
adriano1229 <grigorenko1229@gmail.com>
2025-07-11 18:53:13 +0200

    1_commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styles from './Hero.module.scss';
+import ButtonLink from '../ui/ButtonLink';
+import Container from '../container/Container';
+
+export default function Hero() {
+
+
+  return (
+    <section className={styles.section}>
+      <div className={styles.imageContainer}>
+        <Container>
+          <div className={styles.heroContent}>
+            <h1 className={styles.heroTitle}>
+              Amazing Discounts on Garden Products!
+            </h1>
+            <ButtonLink
+              to="/cart"
+              className={styles.heroBtn}
+              text={'Check out'}
+            />
+          </div>
+        </Container>
+      </div>
+    </section>
+  );
+}
